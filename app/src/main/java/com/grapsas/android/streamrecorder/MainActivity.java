33d7007c85412b4fc218ff58f1e7d8152dbf8091
Grapsas A. Filippos <public001@grapsas.com>
2016-01-22 08:57:05 +0200

    * Add recording view.

--- app/src/main/java/com/grapsas/android/streamrecorder/MainActivity.java
+++ app/src/main/java/com/grapsas/android/streamrecorder/MainActivity.java
@@ -3,10 +3,13 @@ package com.grapsas.android.streamrecorder;
 import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+//import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewStub;
+import android.widget.RelativeLayout;
 
 import com.grapsas.android.streamrecorder.misc.IO;
 import com.grapsas.android.streamrecorder.misc.MyLog;
@@ -15,6 +18,11 @@ import java.io.IOException;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private View recordingView;
+    // TODO: Add elevation
+    private RelativeLayout recordingLayout;
+    private FloatingActionButton fab;
 
     private MediaRecorder recorder;
 
@@ -26,29 +34,64 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = ( Toolbar ) findViewById( R.id.toolbar );
         setSupportActionBar( toolbar );
 
-        FloatingActionButton fab = ( FloatingActionButton ) findViewById( R.id.fab );
+        fab = ( FloatingActionButton ) findViewById( R.id.fab );
         fab.setOnClickListener( new View.OnClickListener() {
             @Override
             public void onClick( View view ) {
-//                Snackbar.make( view, "Replace with your own action", Snackbar.LENGTH_LONG )
-//                        .setAction( "Action", null ).show();
                 if( recorder == null ) {
                     startRecording();
-                    Snackbar.make( view, "Recording started.", Snackbar.LENGTH_LONG )
-                        .setAction( "Action", null ).show();
-                }
-                else {
+//                    Snackbar.make( view, "Recording started.", Snackbar.LENGTH_LONG )
+//                            .setAction( "Action", null ).show();
+                } else {
                     stopRecording();
-                    Snackbar.make( view, "Recording stopped.", Snackbar.LENGTH_LONG )
-                        .setAction( "Action", null ).show();
+//                    Snackbar.make( view, "Recording stopped.", Snackbar.LENGTH_LONG )
+//                            .setAction( "Action", null ).show();
                 }
             }
         } );
 
-
+        this.recordingLayout = (RelativeLayout) findViewById( R.id.recordingLayout );
         MyLog.d( IO.getWorkingDirectory() + "audiorecordtest.3gp" );
     }
 
+
+    private void showBottomLayout() {
+        ViewGroup.LayoutParams params = this.recordingLayout.getLayoutParams();
+        params.height = ViewGroup.LayoutParams.WRAP_CONTENT;
+        this.recordingLayout.setLayoutParams( params );
+    }
+
+    private void hideBottomLayout() {
+        ViewGroup.LayoutParams params = this.recordingLayout.getLayoutParams();
+        params.height = 0;
+        this.recordingLayout.setLayoutParams( params );
+    }
+
+    private void showRecordingView() {
+        // Use recordingView for first time.
+        if( this.recordingView == null ) {
+            this.recordingView = ( ( ViewStub ) findViewById( R.id.stub_import ) ).inflate();
+            this.recordingView.findViewById( R.id.stop ).setOnClickListener( new View.OnClickListener() {
+                @Override
+                public void onClick( View v ) {
+                    stopRecording();
+                }
+            } );
+        }
+        // Reuse recordingView
+        else
+            this.recordingView.setVisibility( View.VISIBLE );
+
+        this.fab.hide();
+        showBottomLayout();
+    }
+
+    private void hideRecordingView() {
+        this.recordingView.setVisibility( View.GONE );
+
+        hideBottomLayout();
+        this.fab.show();
+    }
 
     public void startRecording() {
         recorder = new MediaRecorder();
@@ -64,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
+        this.showRecordingView();
         recorder.start();
     }
 
@@ -71,6 +115,8 @@ public class MainActivity extends AppCompatActivity {
         recorder.stop();
         recorder.release();
         recorder = null;
+
+        this.hideRecordingView();
     }
 
 }
